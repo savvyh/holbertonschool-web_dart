5a7faa76b265a9a7fb34465d5f5bac339c542a9c
savvyh <7277@holbertonstudents.com>
2026-03-20 15:39:54 +0000

    feat: enhance password validation logic to require uppercase, lowercase, and numeric characters

--- 0x02-dart-classes/2-password.dart
+++ 0x02-dart-classes/2-password.dart
@@ -2,13 +2,10 @@ class Password {
     String password = "";
 
     bool isValid() {
-        if (password.length >= 8 && password.length <= 16) {
-            return true;
-        }
-        if (password.contains(new RegExp(r'[A-Za-z]'))) {
-            return true;
-        }
-        if (password.contains(new RegExp(r'[0-9]'))) {
+        if (password.length >= 8 && password.length <= 16 &&
+            password.contains(RegExp(r'[A-Z]')) &&
+            password.contains(RegExp(r'[a-z]')) && 
+            password.contains(RegExp(r'[0-9]'))) {
             return true;
         }
         return false;
