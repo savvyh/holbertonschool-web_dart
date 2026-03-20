d2418f4149678bd3902ddb898dc858b2f56d2711
savvyh <7277@holbertonstudents.com>
2026-03-20 15:46:48 +0000

    feat: add Password class with validation logic and string representation

---
+++ 0x02-dart-classes/3-private_password.dart
@@ -0,0 +1,18 @@
+class Password {
+    String _password = "";
+
+    bool isValid() {
+        if (_password.length >= 8 && _password.length <= 16 &&
+            _password.contains(RegExp(r'[A-Z]')) &&
+            _password.contains(RegExp(r'[a-z]')) && 
+            _password.contains(RegExp(r'[0-9]'))) {
+            return true;
+        }
+        return false;
+    }
+
+    @override
+    String toString() {
+        return "Your Password is: $_password";
+    }
+}
